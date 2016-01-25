ab7425b4e77a023b13fbc585d21c8332830e1df7
johnedstone <johnedstone@gmail.com>
2016-01-24 23:37:14 -0500

    recovering ..

--- wsgi/myproject/lists/views.py
+++ wsgi/myproject/lists/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.conf import settings
 
 def home_page(request):
+    '''Comments here'''
     return HttpResponse('<html><title>To-Do lists</title><body>{}</body></html>'.format('''<pre>boo hoo\n
     whoo hooo</pre>'''))
     #return HttpResponse('<html><title>To-Do lists</title></html>')
