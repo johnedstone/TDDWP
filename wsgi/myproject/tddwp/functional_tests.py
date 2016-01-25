739ae783eab98fc51bf2cbe4c422e063a4e87c3a
johnedstone <johnedstone@gmail.com>
2016-01-25 02:53:20 -0500

    adding tddwp

---
+++ wsgi/myproject/tddwp/functional_tests.py
@@ -0,0 +1,64 @@
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith checks out the new app
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types 'Buy feathers' into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter the page updates and now the page lists her first item
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is stll a text box inviting her to add another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again and now shows both items
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders if the site will remember her lists.   She
+        # the site has generated a unique URL for here -- there is some
+        # explanatory text around this point
+        self.fail('Finish the test!')
+
+        # She visits that URL - her to-do lsit is still there.
+
+        # Satisfiied, she goes back to sleep
+
+if __name__ == '__main__':
+    # unittest.main(warnings='ignore')
+    unittest.main()
