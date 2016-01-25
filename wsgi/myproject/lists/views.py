6310a36b1b8b3ffbb86c346d5e87164a7fc6a63a
johnedstone <johnedstone@gmail.com>
2016-01-24 23:26:51 -0500

    recovered from failed attempt to pull from tddwp

--- wsgi/myproject/lists/views.py
+++ wsgi/myproject/lists/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.conf import settings
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title><body>{}</body></html>'.format('boo hoo'))
+    return HttpResponse('<html><title>To-Do lists</title><body>{}</body></html>'.format('''<pre>boo hoo\n
+    whoo hooo</pre>'''))
     #return HttpResponse('<html><title>To-Do lists</title></html>')
