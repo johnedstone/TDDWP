95462d2c49f16880a2bb4c7a6cb4ab688e9cb605
johnedstone <johnedstone@gmail.com>
2016-01-24 23:57:18 -0500

    adding date

--- wsgi/myproject/lists/views.py
+++ wsgi/myproject/lists/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.conf import settings
 
+now = "{:%B %d, %Y}".format(datetime.now())
+
 def home_page(request):
     '''Comments here'''
-    return HttpResponse('<html><title>To-Do lists</title><body>{}</body></html>'.format('''<pre>boo hoo\n
-    whoo hooo</pre>'''))
+    return HttpResponse('<html><title>To-Do lists</title><body><div>{}</div><div>{}</div></body></html>'.format('''<pre>boo hoo\n
+    whoo hooo</pre>''', now))
     #return HttpResponse('<html><title>To-Do lists</title></html>')
