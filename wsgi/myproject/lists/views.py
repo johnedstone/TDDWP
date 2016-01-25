187fc4fb4ea5fe9bb992ee2737bf3f0ed64c3a27
johnedstone <johnedstone@gmail.com>
2016-01-24 22:13:35 -0500

    testing branching

--- wsgi/myproject/lists/views.py
+++ wsgi/myproject/lists/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.conf import settings
 
-
 def home_page(request):
     return HttpResponse('<html><title>To-Do lists</title><body>{}</body></html>'.format('boo hoo'))
     #return HttpResponse('<html><title>To-Do lists</title></html>')
