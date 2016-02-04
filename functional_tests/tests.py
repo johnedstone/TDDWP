529187fd94f7475822b30b83eaa2a1eb1b911e10
johnedstone <johnedstone@gmail.com>
2016-02-03 22:46:34 -0500

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,10 @@
-import unittest
-import time
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -20,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith checks out the new app
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # import time
         # time.sleep(10)
 
@@ -61,7 +60,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do lsit is still there.
 
         # Satisfiied, she goes back to sleep
-
-if __name__ == '__main__':
-    # unittest.main(warnings='ignore')
-    unittest.main()
