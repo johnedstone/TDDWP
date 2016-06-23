c63b41f98eb28d07e3467857ac523406ba0c9724
Ed Johnstone <johnedstone@gmail.com>
2016-06-22 23:28:29 -0400

    Refactor hard-coded URLs out of templates

--- lists/urls.py
+++ lists/urls.py
@@ -4,6 +4,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^new', views.new_list, name='new_list'),
-    url(r'^(\d+)/$', views.view_list, name='view_lists'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
