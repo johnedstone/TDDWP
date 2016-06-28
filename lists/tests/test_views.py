7e4e19241e79c4ee0636e656bf0043b1ba6a4fc2
Ed Johnstone <johnedstone@gmail.com>
2016-06-27 22:12:30 -0400

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,6 +6,7 @@ from django.utils.html import escape
 from lists.models import Item, List
 
 from lists.views import home_page
+from lists.forms import ItemForm
 
 class NewListTest(TestCase):
 
@@ -102,23 +103,14 @@ class ListViewTest(TestCase):
         self.assertContains(response, expected_error)
 
 class HomePageTest(TestCase):
+    maxDiff = None
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_renders_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-        # request = HttpRequest()
-        # response = home_page(request)
 
-        # expected_html = render_to_string('home.html')
-
-        # self.assertEqual(response.content.decode(), expected_html)
-
-        # response = home_page(request)
-        # self.assertEqual(response.status_code, 302)
-        # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 # vim: ai nu sw=4 ts=4 et sts=4
