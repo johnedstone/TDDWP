0ace40a68745165154037e8c309d3a58ba6853da
Ed Johnstone <johnedstone@gmail.com>
2016-08-10 21:14:05 -0400

    finishing up jQuery tests

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,10 @@
 /* This appears to be removed by QUnit test
  * so I've added it directly to the test
  */
-$('input').on('keypress', function () {
-    $('.has-error').hide();
+jQuery.noConflict();
+jQuery(document).ready(function($) {
+    console.log("Ready two three four");
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
