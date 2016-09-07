66b9a3f70db2f912f06acdcae587d4df06236b19
Ed Johnstone <johnedstone@gmail.com>
2016-09-06 23:09:03 -0400

    ready for QUnit setup and teardown, p 270

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,7 +1,8 @@
-var initialize = function (navigator) {
+var initialize = function (navigator, user, token, urls) {
     jQuery('#id_login').on('click', function ($) {
         navigator.id.request();
     });
+    navigator.id.watch({loggedInUser: user});
 };
 window.Superlists = {
     Accounts: {
