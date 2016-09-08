ed99a991fb61ba960d8419566639905c79f6cee0
Ed Johnstone <johnedstone@gmail.com>
2016-09-07 22:21:46 -0400

    Ready for more nested callbacks, p. 274

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -2,7 +2,16 @@ var initialize = function (navigator, user, token, urls) {
     jQuery('#id_login').on('click', function ($) {
         navigator.id.request();
     });
-    navigator.id.watch({loggedInUser: user});
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function (assertion) {
+            jQuery.post(
+                urls.login,
+                { assertion: assertion, csrfmiddlewaretoken: token }
+            );
+        },
+        onlogout: function () {}
+    });
 };
 window.Superlists = {
     Accounts: {
