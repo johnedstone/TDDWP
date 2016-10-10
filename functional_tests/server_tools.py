3cb7078c588145b69722105a3c9be38cc52bad20
Ed Johnstone <johnedstone@gmail.com>
2016-10-09 23:16:01 -0400

    working on password errors

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -16,6 +16,7 @@ def create_session_on_server(host, email):
     ).decode().strip()
 
 def reset_database(host):
+    subprocess.check_call('whoami')
     subprocess.check_call(
         ['fab', 'reset_database', '--host={}'.format(host)],
         cwd=THIS_FOLDER
