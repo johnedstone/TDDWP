020aea2b95c327222abf0b937321a36d105d19f6
Ed Johnstone <johnedstone@gmail.com>
2016-10-10 22:58:55 -0400

    p.320 .. Using Hierarchical logging config

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -5,19 +5,27 @@ THIS_FOLDER = path.dirname(path.abspath(__file__))
 # Right now, Fabric is only verified on python 2
 # so wrapping this in subprocess
 def create_session_on_server(host, email):
+    ''' fab -i ~/.ssh/aws_superlist.pem deploy:host=ubuntu@superlist-staging.johnedstone-ec2.net'''
     return subprocess.check_output(
         [
             'fab',
-            'creat_session_on_server:email={}'.format(email),
-            '--host={}'.format(host),
-            '--hide=everything,status'
+            '-i',
+            '~/.ssh/aws_superlist.pem',
+            'create_session_on_server:email={}'.format(email),
+            '--host=ubuntu@{}'.format(host),
+            '--hide=everything,status',
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
 
 def reset_database(host):
-    subprocess.check_call('whoami')
     subprocess.check_call(
-        ['fab', 'reset_database', '--host={}'.format(host)],
+        ['fab',
+         '-i',
+         '~/.ssh/aws_superlist.pem',
+         'reset_database',
+         '--host=ubuntu@{}'.format(host),
+         ],
         cwd=THIS_FOLDER
     )
+# vim: ai et sts=4 ts=4 sw=4
