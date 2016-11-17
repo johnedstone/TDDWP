ce633134d9042550d93f376b6a9936abc0cd8010
Ed Johnstone <johnedstone@gmail.com>
2016-11-16 22:30:15 -0500

    up to page, p.335

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class ItemModelTest(TestCase):
 
@@ -56,5 +58,13 @@ class ListModelTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email='a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+    def test_list_owner_is_optional(self):
+        List.objects.create() # should not raise
 
 # vim: ai nu sw=4 ts=4 et sts=4
